d8a1332894ca4fd12f59b4afea4fe02ff5364432
eesha-k <erkurode@gmail.com>
2023-10-03 10:58:16 -0400

    Created mimmaxcalculation class

---
+++ src/MinMaxCalculation.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+import java.util.*;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * A simple class to compute minimum and maximum.
+ */
+public class MinMaxCalculation {
+
+  private static ArrayList<Double> data = new ArrayList<Double>();
+
+  public static void main() {
+
+  }
+
+}
